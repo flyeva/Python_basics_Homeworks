69d65ec92d410eb24a19c383b266b1d714c34ebf
Eva Simetinger <eva.simetinger@gmail.com>
2020-10-23 13:46:44 +0200

    guess the secret number

--- secret_number-8_02.py
+++ secret_number-8_02.py
@@ -4,9 +4,9 @@ guess = input("Guess the srcret number! ")
 
 if guess == "55":
     print("Congratulations, you guessed the secret number!")
-elif "0" <= guess <= "50":
+elif "0" <= guess <= "55":
     print("Try higher. ")
-elif guess > "50":
+elif guess > "55":
     print("Try lower. ")
 else:
     print("The number is positive.")
