dc025937c2b6b812a904e2b5d84ddff0dbaa1293
Eva Simetinger <eva.simetinger@gmail.com>
2020-10-23 11:30:56 +0200

    the mood checker

---
+++ the_mood_checker-8_01.py
@@ -0,0 +1,12 @@
+mood = input("What is your mood today? ")
+
+if mood == "happy":
+    print("Great, good to hear you are well :) ")
+elif mood == "sad":
+    print("Dont't worry, the sun will shine again for you soon. ")
+elif mood == "nervous":
+    print("Just take a deep breath and relax..")
+elif mood == "excited":
+    print("Yeah! :) ")
+else:
+    print("Sorry, I don't recognize this mood.")
