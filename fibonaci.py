5af376d9573bdbcdccfd2386cecb86c5adbe9291
Eva Simetinger <eva.simetinger@gmail.com>
2020-10-24 19:55:02 +0200

    fibonacci - ne dela prav

---
+++ fibonaci.py
@@ -0,0 +1,22 @@
+num = int(input("Enter a number "))
+
+
+def fib(n):
+    a = 0
+    b = 1
+    if n == 1:
+        print(a)
+    elif n == 2:
+        print(b)
+    else:
+        print(a)
+        print(b)
+
+    for i in range(2, n):
+        c = a + b
+        a = b
+        b = c
+        print(c)
+
+
+fib(num)
